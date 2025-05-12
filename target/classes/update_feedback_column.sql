e07da1d6340ced1684fc893f6d0b5e2ce24cae80
auob123 <amjedelsadig@gmail.com>
2025-05-12 23:25:56 +0500

    docker

--- target/classes/update_feedback_column.sql
+++
@@ -1,4 +0,0 @@
--- Migration to fix Data too long for column 'feedback' error
--- Changes the feedback column to LONGTEXT to allow much longer values
-
-ALTER TABLE ai_validation_results MODIFY feedback LONGTEXT;
